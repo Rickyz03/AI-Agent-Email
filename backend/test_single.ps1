16210312946882a29a39007a581cf39d644d2362
Rickyz03 <riccardost02@gmail.com>
2025-09-14 20:46:32 +0200

    Little spaces removed

--- backend/test_single.ps1
+++ backend/test_single.ps1
@@ -2,5 +2,5 @@ $backendHost = "http://127.0.0.1:8001"
 
 # Log feedback: bozza accettata
 Invoke-RestMethod -Uri "$backendHost/feedback?event_type=draft_accepted" `
-    -Method POST -ContentType "application/json" `
-    -Body (@{ metadata = @{ email_id = 1; variant = 0 } } | ConvertTo-Json -Depth 3)
+  -Method POST -ContentType "application/json" `
+  -Body (@{ metadata = @{ email_id = 1; variant = 0 } } | ConvertTo-Json -Depth 3)
