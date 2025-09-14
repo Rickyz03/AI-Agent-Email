72fcbebc7a7cde354207c99bc93a67372a16232c
Rickyz03 <riccardost02@gmail.com>
2025-09-14 11:58:13 +0200

    Created a single test file, for single APIs

---
+++ backend/test_single.ps1
@@ -0,0 +1,6 @@
+$backendHost = "http://127.0.0.1:8001"
+
+# Log feedback: bozza accettata
+Invoke-RestMethod -Uri "$backendHost/feedback?event_type=draft_accepted" `
+    -Method POST -ContentType "application/json" `
+    -Body (@{ metadata = @{ email_id = 1; variant = 0 } } | ConvertTo-Json -Depth 3)
